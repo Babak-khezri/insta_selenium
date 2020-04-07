2990f3ba3b2bd2c2db2944dac6df84c383e78b0a
unknown <babak.khezri7@gamil.com>
2020-04-07 14:14:04 +0430

    Fix some bugs

--- follow.py
+++ follow.py
@@ -2,36 +2,37 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from random import sample
 from time import *
-driver = webdriver.Chrome()
+driver = webdriver.Chrome() # create the driver
 def open_ins():
     driver.get('https://www.instagram.com/')
     enter_acc()
 def enter_acc():
-    sleep(4)
+    sleep(2)
     driver.find_element_by_xpath('//input[@name="username"]').send_keys('selenium__py')
     driver.find_element_by_xpath('//input[@name="password"]').send_keys('1379bk@15')
     driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button/div').click()
-    sleep(2)
+    sleep(3)
     driver.get('https://www.instagram.com/explore/')
     finder()
 def finder():
     name = list("abcdefghijklmnopqrstuvwxyz")
     search = "".join(sample(name,4))
-    sleep(1)
-    driver.find_element_by_xpath('//input[@placeholder="Search"]').send_keys(search)
     sleep(2)
-    lst = driver.find_elements_by_tag_name('a')
+    driver.find_element_by_xpath('//input[@placeholder="Search"]').send_keys(search)
     links = []
-    for i in lst:
-        if search in i.get_attribute('href') and "tags" not in i.get_attribute('href') and "locations" not in i.get_attribute('href'):
-            links.append(i.get_attribute('href'))
+    while len(links) == 0:
+        sleep(0.5)
+        lst = driver.find_elements_by_tag_name('a')
+        for i in lst:
+            if search in i.get_attribute('href') and "tags" not in i.get_attribute('href') and "locations" not in i.get_attribute('href'):
+                links.append(i.get_attribute('href'))
     follow(links)
 def follow(links):
+    sleep(1)
     for i in links:
         driver.get(i)
         sleep(0.5)
-        driver.find_element_by_class_name('_5f5mN').click()
-        print(i)
+        driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[1]/span/span[1]/button').click()
 open_ins()
 sleep(10)
 driver.quit()
