f91347763c5ae45969ac3c3d99190337f5458529
unknown <babak.khezri7@gamil.com>
2020-04-09 04:02:17 +0430

    colors

--- follow.py
+++ follow.py
@@ -7,6 +7,8 @@ win = Tk()
 driver = webdriver.Chrome()  # Create the driver
 enter_user = None
 enter_pass = None
+
+
 def enter_account():  # Enter to the account
     username = enter_user.get()
     password = enter_pass.get()
@@ -20,6 +22,8 @@ def enter_account():  # Enter to the account
     # Go to explorer page to pass the welcome massage
     driver.get('https://www.instagram.com/explore/')
     finder()
+
+
 def finder():  # Search name to find profiles
     name = list("abcdefghijklmnopqrstuvwxyz")
     search = "".join(sample(name, 4))  # Create the word
@@ -29,38 +33,42 @@ def finder():  # Search name to find profiles
     links = []  # Get acoount links
     while len(links) == 0:
         sleep(0.5)
-        lst = driver.find_elements_by_tag_name(
-            'a')  # Find all elements that have link
+        lst = driver.find_elements_by_tag_name('a')  # Find all elements that have link
         for i in lst:  # Just get the account links
             if search in i.get_attribute('href') and "tags" not in i.get_attribute('href') and "locations" not in i.get_attribute('href'):
                 links.append(i.get_attribute('href'))
     follow(links)
+
+
 def follow(links):  # Go to links and follow
     sleep(1)
     for i in links:
         driver.get(i)
         sleep(0.5)
-        f_1 = driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[1]/span/span[1]/button')
-        if len(f_1) == 1:  # Check that account wasnt private
-            f_1[0].click()  # Follow
-def tkinter(): # make gui to get account
+        follow_but = driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[1]/span/span[1]/button')
+        if len(follow_but) == 1:  # Check that account wasnt private
+            follow_but[0].click()  # Follow
+
+
+def tkinter():  # make gui to get account
     win.title("Instagram")
-    win.geometry("700x600")
-    Label(win,text = "<< Instagram BOT >>",fg = "purple",font = ("Aryal",32,'bold')).pack()
-    Label(win,text = "Enter the Username : ",font = ("Aryal",32,'bold')).pack()
+    win.geometry("700x550")
+    Label(win, text="<< Instagram BOT >>", fg="#cc0099",font=("Aryal", 32, 'bold')).pack()
+    Label(win, text="Enter the Username : ",fg = '#003300', font=("Aryal", 32, 'bold')).pack()
     global enter_user
-    enter_user = Entry(win,font = ("Aryal",32,'bold'),bg = "pink")
+    enter_user = Entry(win, font=("Aryal", 32, 'bold'), bg="pink")
     enter_user.pack()
-    Label(win,text = "Enter the Password : ",font = ("Aryal",32,'bold')).pack()
+    Label(win, text="Enter the Password : ",fg = '#003300' ,font=("Aryal", 32, 'bold')).pack()
     global enter_pass
-    enter_pass = Entry(win,font = ("Aryal",32,'bold'),bg = "pink")
+    enter_pass = Entry(win, font=("Aryal", 32, 'bold'), bg="pink")
     enter_pass.pack()
-    Label(win,text = " ",font = ("Aryal",32,'bold')).pack()
-    but = Button(win,text = "ENTER",bg = "green",font = ("Aryal" ,25,'bold'))
-    but.config(bd = 10 ,padx = 25 ,pady = 15 ,command = enter_account ,activebackground = "red")
+    Label(win, text="", font=(32)).pack()
+    but = Button(win, text="LOG IN", bg="#800080", font=("Aryal", 25, 'bold'))
+    but.config(bd=20, padx=25, pady=15,command=enter_account, activebackground="red")
     but.pack()
     mainloop()
-#Start the program
+
+
+# Start the program
 tkinter()
-sleep(10)
 driver.quit()  # Close the site
